b090e74063905bd2eda79d2f39b192999b604f7c
@VIKRANTHDUNDRA <vikranthdundra10@gmail.com>
2024-10-20 13:15:04 -0400

    Question 6 solved

--- ISM6225_Spring_2024_Assignment_2/Program.cs
+++ ISM6225_Spring_2024_Assignment_2/Program.cs
@@ -239,8 +239,34 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return 0; // Placeholder
+                int left = 0;
+                int right = nums.Length - 1;
+
+                // If array is not rotated
+                if (nums[left] <= nums[right])
+                    return nums[left];
+
+                // Binary search
+                while (left < right)
+                {
+                    int mid = left + (right - left) / 2;
+
+                    // If mid element is greater than next element
+                    if (nums[mid] > nums[mid + 1])
+                        return nums[mid + 1];
+
+                    // If mid element is less than previous element
+                    if (nums[mid - 1] > nums[mid])
+                        return nums[mid];
+
+                    // If mid element is greater than left element
+                    if (nums[left] <= nums[mid])
+                        left = mid + 1;
+                    else
+                        right = mid - 1;
+                }
+
+                return nums[0];
             }
             catch (Exception)
             {
